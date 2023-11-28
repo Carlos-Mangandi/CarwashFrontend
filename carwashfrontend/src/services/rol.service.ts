12324c3e389f4d26ce064a244ebb071ccddd17d1
Carlos Mangandí <me22001@esfe.agape.edu.sv>
2023-11-28 15:58:28 -0600

    Adición del Token en el proyecto

--- carwashfrontend/src/services/rol.service.ts
+++ carwashfrontend/src/services/rol.service.ts
@@ -2,17 +2,28 @@
 import axios from "axios";
 import {API_URL} from "../utils/constants"
 import {  IGetRoles } from "../types/rol.types";
+import { GetToken } from "../utils/authData";
 
 export const get_roles = async () => {
     const { data } = await axios.get<{ rol: IGetRoles[] }>(
       API_URL + "/rol",
+      {
+        headers: {
+            Authorization: "Bearer "  + GetToken()
+        } 
+      }
     );
     return data;
 };
 
 export const create_rol = async (type: string)=> {
     const {data} = await axios.post<{ok:boolean, msg: string}>(
-        API_URL + "/rol", {type,}
+        API_URL + "/rol", {type,},
+        {
+            headers: {
+                Authorization: "Bearer"  + GetToken()
+            } 
+          }
     );
     return data
 }
@@ -20,6 +31,11 @@ export const create_rol = async (type: string)=> {
 export const update_rol = async (id: number, type: string) => {
     const {data} = await axios.put<{ok:boolean, msg: string}>(
         API_URL + "/rol/" + id, {type, },
+        {
+            headers: {
+                Authorization: "Bearer "  + GetToken()
+            } 
+        }
     );
     return data
 }
@@ -27,14 +43,23 @@ export const update_rol = async (id: number, type: string) => {
 export const delete_rol = async (id: number) => {
     const {data} = await axios.delete<{ok: boolean, msg:string}>(
         API_URL + "/rol/" + id,
+        {
+            headers: {
+                Authorization: "Bearer "  + GetToken()
+            } 
+        }
     );
     return data
 }
 
 export const get_rol_id = async (id: number) => {
     const { data } = await axios.get<{ roles: IGetRoles[] }>(
-      API_URL + "/rol" + id,
-     
+        API_URL + "/rol" + id,
+        {
+            headers: {
+                Authorization: "Bearer "  + GetToken()
+            } 
+        }
     );
     return data;
   };
--- carwashfrontend/src/services/user.service.ts
+++ carwashfrontend/src/services/user.service.ts
@@ -1,14 +1,26 @@
 import axios from 'axios';
 import { API_URL } from '../utils/constants';
 import {ICreateUser, IGetUsers, IUpdateUser} from '../types/user.types'
+import { GetToken } from '../utils/authData';
 
 export const get_users = async () => {
-    const response = await axios.get<{user: IGetUsers[]}>(`${API_URL}/user`);
+    const response = await axios.get<{user: IGetUsers[]}>(`${API_URL}/user`,
+     {
+        headers: {
+            Authorization: "Bearer "  + GetToken()
+        } 
+      });
     return response.data;
 }
 
 export const create_user = async (user: ICreateUser) => {
-    const response =await axios.post(`${API_URL}/user`, user);
+    const response =await axios.post(`${API_URL}/user`, user,
+    {
+        headers: {
+            Authorization: "Bearer "  + GetToken()
+        } 
+      }
+    );
     return response.data;
 }
 
